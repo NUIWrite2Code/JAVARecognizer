bcb3fccceea7cbc1c6f312e903690c9c0a2ff00a
Pedro Guillermo Feijoo Garcia <pfeijoo@Pedros-MacBook-Pro.local>
2020-03-15 22:17:18 -0400

    modified GUI to include Check button and some extra detail on the assessment

--- build/es5-bundled/service-worker.js
+++ build/es5-bundled/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","335138ab3e6a3a884e65d0308c91b172"]];
+var precacheConfig = [["index.html","1384469f255ec50616954b8f99a51c9c"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
